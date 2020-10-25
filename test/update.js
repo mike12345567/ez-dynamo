b731d2d503df53a20f19daeaf959295336263739
mike12345567 <me@michaeldrury.co.uk>
2020-10-25 20:02:06 +0000

    Lint.

--- test/update.js
+++ test/update.js
@@ -7,7 +7,7 @@ const ezDynamo = require("../index"),
 
 ezDynamo.configureFromArg("eu-west-1", "http://localhost:8123", "mock", "mock");
 
-let table
+let table;
 
 describe("Test some basic updates", () => {
   it("should successfully create a table and entry", async () => {
@@ -17,21 +17,30 @@ describe("Test some basic updates", () => {
         test: schema.string(),
         string: schema.string(),
         number: schema.number(),
-        set: schema.array(),
+        set: schema.array()
       })
       .writeCapacity(2)
       .readCapacity(2);
     await table.validate().create();
-    await table.put({
-      test: "TEST",
-      string: "TEST",
-      number: 1
-    }).run();
+    await table
+      .put({
+        test: "TEST",
+        string: "TEST",
+        number: 1
+      })
+      .run();
   });
 
   it("should allow a basic update of the 'string' prop", async () => {
-    await table.update().primary("TEST").set("string", "UPDATE").run();
-    const obj = await table.get().primary("TEST").run();
+    await table
+      .update()
+      .primary("TEST")
+      .set("string", "UPDATE")
+      .run();
+    const obj = await table
+      .get()
+      .primary("TEST")
+      .run();
     obj.string.should.equal("UPDATE");
   });
 });
